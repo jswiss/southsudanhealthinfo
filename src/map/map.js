907bcda399c47d768d10648de2371dc23ad1921b
Joshua Swiss <swissjoshua@gmail.com>
2016-10-28 14:38:50 +0100

    added favicon folder to gitignore

---
+++ src/map/map.js
@@ -0,0 +1,23 @@
+var map;
+
+function init() {
+  //initiate Leaflet Map
+  map = new L.map('cartodb-map', {
+    center: [0,0],
+    zoom: 2
+  });
+
+var layerUrl = 'http://documentation.carto.com/api/v2/viz/836e37ca-085a-11e4-8834-0edbca4b5057/viz.json';
+
+cartodb.createLayer(map, layerUrl)
+  .addTo(map)
+  .on('done', function(layer) {
+  }).on('error', function() {
+    //log err
+    console.log('error: ', error);
+  });
+
+}
+
+
+
